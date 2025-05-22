0028f1e51e248865be23254fc2c56828ff3451a0
rahimi <reimy94@gmail.com>
2025-05-22 16:56:44 +0800

    move semantics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -6,6 +6,17 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec
 }
 
+// by using this fn, we clone vec
+// inside fn instead of manually
+// when we call the fn
+fn fill_vec_reference(vec: &[i32]) -> Vec<i32> {
+    // convert slice into vec
+    let mut v = vec.to_vec();
+    v.push(88);
+
+    v
+}
+
 fn main() {
     // You can optionally experiment here.
 }
@@ -20,7 +31,7 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
